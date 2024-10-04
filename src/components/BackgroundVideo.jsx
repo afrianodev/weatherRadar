8e7468e1974fc58250423767dc092aae6a2e900f
piperiao <andresfeliperianom@gmail.com>
2024-10-04 17:38:06 -0600

    fix background and layout

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -5,7 +5,7 @@ const BackgroundVideo = () => {
   return (
     <div className="absolute inset-0 overflow-hidden z-[-1] pointer-events-none">
         <iframe
-          className="absolute -top-36 md:-top-36 -left-60 md:left-0 w-[300%] lg:w-[200%] h-[800px]"
+          className="relative left-[-1100px] w-[250%] h-[150%]"
           src="https://www.youtube.com/embed/Y8ACyHYsb6Q?autoplay=1&mute=1&controls=0&loop=1&playlist=Y8ACyHYsb6Q"
           title="YouTube video"
           frameBorder="0"
