d41b86f02c9b7847a6bf970a60e50a2a0dd3dff3
pipe riaño <andresfeliperianom@gmail.com>
2025-01-14 16:20:42 -0600

    add the spin loading, center the layout and add dynamic change for uv index

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,10 @@ import Footer from './components/Footer';
 
 const App = () => {
   return (<>
-    <div className='w-full h-[100vh] flex flex-col justify-between'>
+    <div className='w-full h-[100vh] flex flex-col justify-between items-center'>
       <BackgroundVideo />
-      <div className="relative flex flex-col items-center w-full">
-        <div className='flex mt-10 md:mt-8 justify-center items-center gap-2'>
+      <div className="relative flex flex-col items-center w-full h-full mt-16">
+        <div className='flex justify-center items-center gap-2'>
           <img src="https://i.imgur.com/JQZSwXl.png" alt="weather radar logo" className='w-14' />
           <h1 className='font-bold text-4xl bg-gradient-to-r from-customDarkPurple to-customPurple bg-clip-text text-transparent'>Weather<span className='bg-gradient-to-r from-customPurple to-customOrange bg-clip-text text-transparent'>Radar</span></h1>
         </div>
